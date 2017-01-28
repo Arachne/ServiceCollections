0c2ba6a796f0f24e377f8445f4954e0a12da3fb8
Jáchym Toušek <enumag@gmail.com>
2017-01-28 08:59:25 +0100

    Add strict types

--- tests/integration/src/Fixtures/TestExtension.php
+++ tests/integration/src/Fixtures/TestExtension.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Tests\Integration\Fixtures;
 
 use Arachne\ServiceCollections\DI\ServiceCollectionsExtension;
--- tests/unit/src/DI/ServiceCollectionsExtensionTest.php
+++ tests/unit/src/DI/ServiceCollectionsExtensionTest.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Tests\Unit\DI;
 
 use Arachne\ServiceCollections\DI\ServiceCollectionsExtension;
